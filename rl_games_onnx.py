28d799887bdbe5a47ab5a2b8a85d2537bae6595b
Roman Furko <furkoroman95@gmail.com>
2024-08-23 22:46:15 -0700

    Add onnx inference for rl_games model that is trained to exploit CFR model

---
+++ rl_games_onnx.py
@@ -0,0 +1,49 @@
+import yaml
+import argparse
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from rl_games.torch_runner import Runner
+
+
+class ModelWrapper(torch.nn.Module):
+    def __init__(self, model):
+        super(ModelWrapper, self).__init__()
+        self._model = model
+
+    def forward(self, x):
+        input_dict = {}
+        input_dict["obs"] = self._model.norm_obs(x)
+        logits = self._model.a2c_network(input_dict)["logits"]
+        probs = F.softmax(logits, dim=-1)
+        return probs
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", required=True, help="path to config")
+    parser.add_argument(
+        "-m", "--model", required=True, help="model checkpoint", type=str
+    )
+
+    args = vars(parser.parse_args())
+    config_name = args["file"]
+    with open(config_name, "r") as stream:
+        config = yaml.safe_load(stream)
+    runner = Runner()
+    runner.load(config)
+
+    agent = runner.create_player()
+    agent.restore(args["model"])
+
+    inputs = np.random.rand(1, 31).astype(np.float32)
+    torch.onnx.export(
+        ModelWrapper(agent.model),
+        torch.tensor(inputs),
+        "rl_games_model.onnx",
+        input_names=["obs"],
+        output_names=["logits"],
+        opset_version=11,
+    )
